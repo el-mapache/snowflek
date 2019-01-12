d53a82646dcacacb9e16ef26b34354e2ffae7946
Adam Biagianti <adam.biagianti@gsa.gov>
2019-01-12 10:44:25 -0800

    Adds button component, abstracts form component
    * Use new form component in sign in, sign up pages

--- src/pages/sign-in.js
+++ src/pages/sign-in.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Formik } from 'formik';
 import { signInAction } from '../actions/auth';
 import Fieldset from '../components/fieldset';
-import Message from '../components/message';
+import Form from '../components/form';
 
 const mapStateToProps = ({ auth }) => ({
   errors: auth.errors
@@ -12,48 +11,10 @@ const mapDispatchToProps = dispatch => ({
   handleSignIn: signInAction(dispatch)
 });
 
-const getForm = component => el => component.form = el;
-
-/**
- * Formats error messages in a reasonably sensible way
- * Handles case where error message might be a form level
- * error or a field level error.
- *
- * Prepends name of field to server generated error in the
- * latter case, and returns only the server error message in
- * the former.
-*/
-const friendlyFormError = (formFields, maybeFieldName, message) => {
-  const formHasField = Object.keys(formFields).includes(maybeFieldName);
-
-  return formHasField ?
-    `${maybeFieldName} ${message}` :
-    message;
-};
-
 class SigninPage extends React.Component {
   state = {
     email: '',
     password: '',
-  }
-
-  componentDidUpdate() {
-    const errorsFromServer = Object.entries(this.props.errors);
-    // TODO: this can all be encapsulated in a component
-    // will want the form to take prefixes as well, in case nested errors are needed
-    const formattedErrors = errorsFromServer.reduce((memo, [name, message]) => {
-      return { 
-        ...memo,
-        [name]: friendlyFormError(this.form.fields, name, message),
-      };
-    }, {});
-
-    if (!Object.keys(formattedErrors).length) {
-      this.form.resetForm();
-    }
-
-    this.form.setSubmitting(false);
-    this.form.setErrors(formattedErrors);
   }
 
   handleSubmit = (values) => {
@@ -64,34 +25,23 @@ class SigninPage extends React.Component {
     return (
       <section id="sign-up">
         <h2>Welcome back! Sign in to your account</h2>
-        <Formik
+        <Form
+          button="Sign in"
+          errors={this.props.errors}
           initialValues={this.state}
           onSubmit={this.handleSubmit}
-          ref={getForm(this)}
         >
-          {({ handleSubmit, isSubmitting, errors }) => {
-            return (
-              <form onSubmit={handleSubmit}>
-                <Message message={errors.form} />
-                <Fieldset
-                  label="Email"
-                  name="email"
-                  type="email"
-                />
-                <Fieldset
-                  label="Password"
-                  name="password"
-                  type="password"
-                />
-                <div>
-                  <button type="submit" disabled={isSubmitting}>
-                    Sign in
-                  </button>
-                </div>
-              </form>
-            );
-          } }
-        </Formik>
+          <Fieldset
+            label="Email"
+            name="email"
+            type="email"
+          />
+          <Fieldset
+            label="Password"
+            name="password"
+            type="password"
+          />
+        </Form>
       </section>
     );
   }
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Formik } from 'formik';
 import { signUpAction } from '../actions/auth';
 import Fieldset from '../components/fieldset';
+import Form from '../components/form';
 
 const mapStateToProps = ({ auth }) => ({
   errors: auth.errors
@@ -12,28 +12,10 @@ const mapDispatchToProps = dispatch => ({
   handleSignUp: signUpAction(dispatch)
 });
 
-const getForm = component => el => component.form = el;
-
 class SignupPage extends React.Component {
   state = {
     email: '',
     password: ''
-  }
-
-  componentDidUpdate() {
-    const serverErrors = this.props.errors;
-    // TODO: this can all be encapsulated in a component
-    // will want the form to take prefixes as well, in case nested errors are needed
-    const formattedErrors = Object.entries(serverErrors).reduce((memo, [name, message]) => {
-      return { 
-        ...memo,
-        [name]: `${name} ${message}`,
-      };
-    }, {});
-    
-    // TODO: why do you have to manage this yourself??
-    this.form.setSubmitting(false);
-    this.form.setErrors(formattedErrors)
   }
 
   handleSubmit = (values) => {
@@ -43,39 +25,28 @@ class SignupPage extends React.Component {
   render() {
     return (
       <section id="sign-up">
-        <h2>Please create an account</h2>
-        <Formik
+        <h2>Hi, looks like you're new here!</h2>
+        <Form
+          button="Create your account"
+          errors={this.props.errors}
           initialValues={this.state}
           onSubmit={this.handleSubmit}
-          ref={getForm(this)}
         >
-          {({ handleSubmit, isSubmitting }) => {
-            return (
-              <form onSubmit={handleSubmit}>
-                <Fieldset
-                  label="Enter your email"
-                  name="email"
-                  type="email"
-                />
-                <Fieldset
-                  label="Choose a password"
-                  name="password"
-                  type="password"
-                />
-                <div>
-                  <button type="submit" disabled={isSubmitting}>
-                    Sign up!
-                  </button>
-                </div>
-              </form>
-            );
-          } }
-        </Formik>
+          <Fieldset
+            label="Enter your email"
+            name="email"
+            type="email"
+          />
+          <Fieldset
+            label="Choose a password"
+            name="password"
+            type="password"
+          />
+        </Form>
       </section>
     );
   }
 }
-
 
 export { SignupPage };
 export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
